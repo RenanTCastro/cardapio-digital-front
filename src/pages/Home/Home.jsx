d26747d8d10e2b5cc9494f5a4994f170062184f3
RenanTCastro <renancastrotut@gmail.com>
2023-10-24 15:16:48 -0300

    criado componente de botoes, import de icones e inicio da construcao da pagina home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { HomeContainer, TitleContainer, Title, ButtonSettings, ButtonLogout } from "./styles";
+
+import LogoutIcon from "../../assets/logout-icon.svg";
+import SettingIcon from "../../assets/setting-icon.svg";
+
+import ButtonCM from "../../components/ButtonCM/ButtonCM";
+
+const Home = () => {
+    return(
+        <HomeContainer>
+           <TitleContainer>
+                <Title>Bem vindo(a), D'Itália</Title>
+                <ButtonSettings>
+                    <img src={SettingIcon} alt="icone sair"/>
+                </ButtonSettings>
+            </TitleContainer>
+            <ButtonLogout>
+                <img src={LogoutIcon} alt="icone sair"/>
+                Sair
+            </ButtonLogout>
+            <ButtonCM>Nova refeição</ButtonCM>
+            <ButtonCM>Gerar QR code</ButtonCM>
+        </HomeContainer>
+    )
+}
+
+export default Home;
