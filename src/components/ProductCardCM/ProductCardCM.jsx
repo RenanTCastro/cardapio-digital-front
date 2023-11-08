25054d855565dbc0fbb01ba0a06de42bf2b8e640
IuryBenicio <iury620@gmail.com>
2023-11-08 16:24:56 -0300

    rota do cardápio para o produto

--- src/components/ProductCardCM/ProductCardCM.jsx
+++ src/components/ProductCardCM/ProductCardCM.jsx
@@ -1,15 +1,18 @@
 import { MenuContainer, MenuItem, MenuItemText } from "./styles"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { ProductsModels as MenuItemModel } from "../../Models/ProductModels"
 
 function ProductCard(){
+    const navigate = useNavigate();
+
+
     return(
         <section>
             <MenuContainer>
                 <h3>Cardápio</h3>
                 <ul>
                     {MenuItemModel.map((e)=>(
-                        <MenuItem key={e.Id}>
+                        <MenuItem onClick={()=>navigate(`/Product/${e.Id}`)} key={e.Id}>
                             <img src={e.Image}/>
                             <MenuItemText>
                                 <Link to='#'><span>{e.Title}</span>
